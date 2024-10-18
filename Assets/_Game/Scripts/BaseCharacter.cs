f59ddc0e89a0c4c3c6a2e4cded4ef789723139a4
VuLinh <vumanhlinh16111995@gmail.com>
2024-10-18 10:19:20 +0700

    ~ BaseCharacter

--- Assets/_Game/Scripts/BaseCharacter.cs
+++ Assets/_Game/Scripts/BaseCharacter.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class BaseCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] protected int id;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int Id => id;
 }
